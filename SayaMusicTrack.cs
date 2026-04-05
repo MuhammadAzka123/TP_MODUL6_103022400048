643aac97bd59d786677b2879293d3f9e85610d5a
MuhammadAzka123 <azkalubis@gmail.com>
2026-04-05 13:28:34 +0700

    menambahkan class SayaMusicTrack

---
+++ SayaMusicTrack.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP_MODUL6_103022400048
+{
+    public class SayaMusicTrack
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaMusicTrack(string title)
+        {
+            Random rand = new Random();
+            this.id = rand.Next(10000, 99999);
+
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            this.playCount += count;
+        }
+
+        public void PrintTrackDetails()
+        {
+            Console.WriteLine("ID: " + id);
+            Console.WriteLine("Title: " + title);
+            Console.WriteLine("Play Count: " + playCount);
+        }
+    }
+}
